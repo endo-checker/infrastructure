150016176e5e209876b0143140fad7526c9f4c4a
el-zacharoo <you@example.com>
2023-03-15 14:53:10 +1300

    changed id

--- modules/mongodb/main.tf
+++ modules/mongodb/main.tf
@@ -15,12 +15,12 @@ resource "mongodbatlas_project" "app" {
 
   # developers
   teams {
-    team_id    = "6257359618051e14ee22e3d4"
+    team_id    = "640fbb3d3b70804145eb8308"
     role_names = ["GROUP_DATA_ACCESS_READ_WRITE"]
   }
   # administrators
   teams {
-    team_id    = "6257360aa5bbe76a27115a5f"
+    team_id    = "640fbb3d3b70804145eb8308"
     role_names = ["GROUP_OWNER"]
   }
 
